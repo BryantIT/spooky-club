851df94108f05d5d20f0ac6a17cd22f462b900de
Bryant Richards <dragonseyeimages@gmail.com>
2021-12-26 21:41:34 -0600

    basic style, header

---
+++ src/Styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+// Profile Main
+export const Wrapper = styled.div`
+  background: rgba(0,0,0,0.5);
+  height: 100%;
+`
+
+export const Pages = styled.div`
+  width: 960px;
+  height: 100%;
+  padding: 0;
+  margin: 0 auto;
+`
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Wrapper } from './Styles'
+
+const Header = () => {
+  return (
+    <Wrapper>
+        <h1>Myst & Orb Society</h1>
+    </Wrapper>
+  )
+}
+
+export default Header
