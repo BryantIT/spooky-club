371d77466e0072d6cb0e4a9ddb94722833d40bc4
Bryant Richards <dragonseyeimages@gmail.com>
2021-12-28 19:22:03 -0600

    firebase examples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useEffect, useState } from "react"
 // import Landing from "./pages/landing/Landing"
-import { AuthProvider } from './auth/UserAuth'
 import Main from "./pages/Main/Main"
 import Navbar from './components/navbar/Navbar'
 // import { Wrapper, Pages } from "./Styles"
 import "./App.css"
 import { Routes, Route, Link } from "react-router-dom"
+import { db } from './firebase-config'
+import { collection, getDocs, addDoc, getDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore'
 
 function App() {
+  const [testName, setTestName] = useState('Bob')
+  const [test, setTest] = useState([])
+  const [single, setSingle] = useState(null)
+  const testRef = collection(db, 'test')
+  const testID = 'RbbfVf4C6de3g8V4NNjC'
+
+  useEffect(() => {
+    // How to get all documents in a collection
+    const getTest = async () => {
+      const data = await getDocs(testRef)
+      setTest(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+    getTest()
+  }, [])
+
+  // How to get single doc
+  const setDoc = async () => {
+    const singleRef = await doc(db, 'test', testID)
+    getDoc(singleRef)
+    .then((doc) => {
+      setSingle(doc.data())
+    })
+  }
+
+
+  // How to create a document
+  const createTest = async () => {
+    await addDoc(testRef, { name: testName })
+  }
+
+  // How to update a doc
+  const updateTest = async () => {
+    const testDoc = doc(db, 'test', testID)
+    const newFields = {name: 'Bryant'}
+    await updateDoc(testDoc, newFields)
+  }
+
+  // How to delete a doc
+  const deleteTest = async () => {
+    const testDoc = doc(db, 'test', testID)
+    await deleteDoc(testDoc)
+  }
   return (
     <Fragment>
-      <AuthProvider>
-        <Navbar />
-        <Routes>
-          {/* <Route path="/" element={<Landing />} /> */}
-          {/* <Route path="/" element={<Main />} /> */}
-        </Routes>
-      </AuthProvider>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={<Landing />} /> */}
+        <Route path="/" element={<Main />} />
+      </Routes>
+      <button onClick={deleteTest}>TEST</button>
     </Fragment>
   )
 }
