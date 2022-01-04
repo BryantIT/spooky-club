60b8d65f895416f7928f9a3647816a2e023b3340
Bryant Richards <dragonseyeimages@gmail.com>
2022-01-04 11:22:06 -0600

    splash screen change

--- src/pages/splash/SplashScreen.js
+++ src/pages/splash/SplashScreen.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react"
-import "./styles.css"
+import styles from "./styles.module.css"
 
 const SplashScreen = ({ endSplash }) => {
   const [showButton, setShowButton] = useState(false)
+  const {
+    container,
+    content,
+    frame1,
+    frame2,
+    frame3,
+    frame4,
+    frame5,
+    btn
+  } = styles
 
   useEffect(() => {
     setTimeout(() => {
@@ -10,18 +20,18 @@ const SplashScreen = ({ endSplash }) => {
     }, 15000)
   }, [])
   return (
-    <div className="container">
-      <div className="content">
-        <h2 className="frame-1">We are believers of the unknown</h2>
-        <h2 className="frame-2">We are paranormal investigators</h2>
-        <h2 className="frame-3">We are here to help</h2>
-      <h2 className="frame-4">If you are ready</h2>
-        <h2 className="frame-5">
+    <div className={container}>
+      <div className={content}>
+        <h2 className={frame1}>We are believers of the unknown</h2>
+      <h2 className={frame2}>We are paranormal investigators</h2>
+    <h2 className={frame3}>We are here to help</h2>
+  <h2 className={frame4}>If you are ready</h2>
+<h2 className={frame5}>
           <span>We are the Myst and Orb Society</span>
           <br />
         {
           showButton ?
-          <button className='btn' onClick={endSplash}>Enter Here</button> : null
+          <button className={btn} onClick={endSplash}>Enter Here</button> : null
         }
         </h2>
       </div>
