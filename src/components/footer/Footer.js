2130f6de521fcf40657e5483510f58760efdc162
Bryant Richards <dragonseyeimages@gmail.com>
2022-01-06 13:00:11 -0600

    work on footer css to modules

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React from "react"
+import styles from "./styles.module.css"
 
 const Footer = () => {
+  const { footer, left, links, companyName, center, right } = styles
   return (
-    <h1>Hello Footer</h1>
+    <footer className={footer}>
+      <div className={left}>
+        <h3>
+          WebDev<span>Trick</span>
+        </h3>
+
+        <p className={links}>
+          <a href="#">Home</a>·<a href="#">Blog</a>·<a href="#">Pricing</a>·
+          <a href="#">About</a>·<a href="#">Faq</a>·<a href="#">Contact</a>
+        </p>
+
+        <p className={companyName}>webdevtrick &copy; 2019</p>
+      </div>
+
+      <div className={center}>
+        <div>
+          <i className="fa fa-map-marker"></i>
+          <p>
+            <span>21 Revolution Street</span> Delhi, India
+          </p>
+        </div>
+
+        <div>
+          <i className="fa fa-phone"></i>
+          <p>+1 555 123456</p>
+        </div>
+
+        <div>
+          <i className="fa fa-envelope"></i>
+          <p>
+            <a href="mailto:support@company.com">contact@webdevtrick.com</a>
+          </p>
+        </div>
+      </div>
+
+      <div className={right}>
+        <p className="footer-company-about">
+          <span>About the company</span>
+          Web Dev Trick is a blog for web designers, graphic designers, web
+          developers &amp; SEO Learner.
+        </p>
+
+        <div className="footer-icons">
+          <a href="#">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-github"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
