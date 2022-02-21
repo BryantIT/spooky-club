5eb5c39bdb725fd56ef9e554e6e0fd6351de5648
Bryant Richards <dragonseyeimages@gmail.com>
2022-02-21 16:56:35 -0600

    redirect and cleanup after submit

--- src/pages/profiles/ProfileDetails.js
+++ src/pages/profiles/ProfileDetails.js
@@ -1,12 +1,17 @@
 import React, { useState, useRef, useEffect } from "react";
 import styles from "./styles.module.css";
+import { useNavigate } from "react-router-dom";
+import Success from '../../components/common/Success'
+// Firebase
 import { useAuth } from "../../auth/UserAuth";
 import { db, storage } from "../../firebase-config";
 import { ref, getDownloadURL, uploadBytesResumable, deleteObject } from "firebase/storage";
+import { doc, updateDoc, getDoc } from "firebase/firestore";
 import { DateTime } from "luxon";
 
 const ProfileDetails = () => {
-  const { currentUser, userInfo } = useAuth();
+  const navigate = useNavigate();
+  const { currentUser, userInfo, refreshUser } = useAuth();
   const {
     container,
     formRow,
@@ -21,6 +26,7 @@ const ProfileDetails = () => {
 
   } = styles;
   const inputFile = useRef();
+  const [userData, setUserData] = useState()
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [city, setCity] = useState("");
@@ -31,6 +37,22 @@ const ProfileDetails = () => {
   const [imageOld, setImageOld] = useState()
   const [bio, setBio] = useState("");
   const [imageURL, setImageURL] = useState()
+  const [updatedOn, setUpdatedOn] = useState()
+  const [saving, setSaving] = useState(false)
+  const successMessage = "Profile Successfully Updated"
+
+  useEffect(() => {
+    if(currentUser) {
+      const setUserData = async () => {
+        const userDoc = await doc(db, 'users', currentUser.uid)
+        getDoc(userDoc)
+        .then((doc) => {
+          setUserData(doc.data())
+        })
+      }
+      setUserData()
+    }
+  }, [currentUser])
 
   const getImageUrl = () => {
     const storageRef = ref(storage, `profileImages/${imageSrc}`);
@@ -41,24 +63,33 @@ const ProfileDetails = () => {
   }
 
   useEffect(() => {
-    getImageUrl()
+    if(imageSrc) {
+      getImageUrl()
+    }
   }, [imageSrc])
 
+  const setUserInfo = () => {
+    setFirstName(userInfo.firstName)
+    setLastName(userInfo.lastName)
+    setCity(userInfo.city)
+    setState(userInfo.state)
+    setZip(userInfo.zip)
+    setInfo(userInfo.info)
+    setImageSrc(userInfo.profileImage)
+    setImageOld(userInfo.profileImage)
+    setBio(userInfo.bio)
+  }
+
   useEffect(() => {
+    const time = DateTime.now().ts
+    setUpdatedOn(time)
     if (userInfo) {
-      setFirstName(userInfo.firstName)
-      setLastName(userInfo.lastName)
-      setCity(userInfo.city)
-      setState(userInfo.state)
-      setZip(userInfo.zip)
-      setInfo(userInfo.info)
-      setImageSrc(userInfo.profileImage)
-      setImageOld(userInfo.profileImage)
-      setBio(userInfo.bio)
+      setUserInfo()
     }
   }, [userInfo])
 
   const uploadeImage = async (file, imageName) => {
+    setSaving(true)
     const storageRef = ref(storage, `profileImages/${imageName}`);
     const uploadTask = uploadBytesResumable(storageRef, file);
 
@@ -78,6 +109,7 @@ const ProfileDetails = () => {
   };
 
   const deleteOldFile = () => {
+    setSaving(true)
     const deleteRef = ref(storage, `profileImages/${imageOld}`)
     deleteObject(deleteRef)
     .then(() => {
@@ -90,6 +122,9 @@ const ProfileDetails = () => {
 
   const handleImageChange = (event) => {
     const image = URL.createObjectURL(event.target.files[0])
+    const date = DateTime.now()
+    const imageName = date + event.target.files[0].name;
+    setImageSrc(imageName)
   }
 
   const handleChange = (event) => {
@@ -102,24 +137,61 @@ const ProfileDetails = () => {
       case 'lastName':
       setLastName(value)
       break
+      case 'city':
+      setCity(value)
+      break
+      case 'state':
+      setState(value)
+      break
+      case 'zip':
+      setZip(value)
+      break
+      case 'info':
+      setInfo(value)
+      break
+      case 'bio':
+      setBio(value)
+      break
       default:
       break
     }
   }
 
+  const updateUser = async () => {
+    const userDoc = doc(db, "users", currentUser.uid);
+    const userObject = {
+      bio: bio,
+      city: city,
+      firstName: firstName,
+      info: info,
+      lastName: lastName,
+      profileImage: imageSrc,
+      state: state,
+      zip: zip,
+      updatedOn: updatedOn
+     };
+    await updateDoc(userDoc, userObject)
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    if (inputFile) {
+    setSaving(true)
+    console.log(saving)
+    const file = event.target.profileImage.files[0];
+    console.log(imageSrc)
+    if (inputFile && imageOld !== imageSrc) {
       const image = URL.createObjectURL(event.target.profileImage.files[0]);
       const file = event.target.profileImage.files[0];
-      const date = DateTime.now();
-      const imageName = date + file.name;
-      setImageSrc(imageName)
-      uploadeImage(file, imageName)
+      uploadeImage(file, imageSrc)
       .then(
         deleteOldFile()
       )
     }
+    updateUser()
+    setTimeout(() => {
+      navigate("/profile")
+    }, 1000)
+    refreshUser()
   }
 
   return (
@@ -153,9 +225,27 @@ const ProfileDetails = () => {
           </div>
           <div className={formRow}>
             <div className={locationWrapper}>
-              <input type="text" value={city} placeholder="City" />
-              <input type="text" value={state} placeholder="State" />
-              <input type="text" value={zip} placeholder="Zipcode" />
+              <input
+                type="text"
+                value={city}
+                placeholder="City"
+                name='city'
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                value={state}
+                placeholder="State"
+                name='state'
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                value={zip}
+                placeholder="Zipcode"
+                name='zip'
+                onChange={handleChange}
+               />
             </div>
           </div>
           <div className={formRow}>
@@ -174,7 +264,11 @@ const ProfileDetails = () => {
           <div className={formRow}>
             <h4 className={h4Color}>Your Role</h4>
             <div className={inputGroup}>
-              <select name="info">
+              <select
+                name="info"
+                value={info}
+                onChange={handleChange}
+                >
                 <option value="investigator">An Investigator</option>
                 <option value="seeking">Seeking an Investigator</option>
                 <option value="fan">Just a Fan</option>
@@ -185,12 +279,19 @@ const ProfileDetails = () => {
           <div className={formRow}>
             <h4 className={h4Color}>Bio</h4>
             <div className={inputGroup}>
-              <textarea value={bio}></textarea>
+              <textarea
+                value={bio}
+                name='bio'
+                onChange={handleChange}
+                ></textarea>
             </div>
           </div>
           <div className={inputGroup}>
             <div className={submitWrapper}>
-              <input type='submit' />
+              {
+                saving ? <Success message={successMessage} /> : null
+              }
+              <input type='submit' disabled={saving}/>
             </div>
           </div>
         </form>
